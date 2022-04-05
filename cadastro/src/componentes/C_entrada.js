6f968d5c79418089d34cf9b3b5aa7d4ff125a4fb
Cicero Junior <cicerojunior9999@gmail.com>
2022-04-05 15:41:21 -0300

    correções

--- cadastro/src/componentes/C_entrada.js
+++ cadastro/src/componentes/C_entrada.js
@@ -4,20 +4,16 @@ const Input = styled.input`
 font-size: 15px;
 border: 2px solid #3A3A3A69;
 padding: 1.0em;
-padding-right: 12.4em;
+width: 24.2em;
 border-radius 6px;
 
 ${props => props.I_email && css`
 margin-left: 7em;
-padding: 1.0em;
-padding-right: 12.4em;
 `}
 
 ${props => props.I_senha && css`
 margin-left: 7em;
 margin-top: 1px;
-padding: 1.0em;
-padding-right: 12.4em;
 `}
 
 `
